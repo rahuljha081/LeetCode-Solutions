90033e45f7fbc565d07c58ba7637526da6527c49
Rahul Jha <rahuljhads556@gmail.com>
2026-07-02 13:06:26 +0530

    Time: 54 ms (34.42%) | Memory: 48.1 MB (13.74%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -2,11 +2,11 @@ class Solution {
     class Pair{
         int row;
         int col;
-        int wt;
-        Pair(int row,int col,int wt){
+        int distance;
+        Pair(int row,int col,int distance){
             this.row=row;
             this.col=col;
-            this.wt=wt;
+            this.distance=distance;
         }
     }
     int rowDirection[]={0,0,-1,1};
@@ -16,34 +16,35 @@ class Solution {
         return false;
     }
     public int minimumEffortPath(int[][] heights) {
-        int dist[][]=new int[heights.length][heights[0].length];
-        for(int i=0;i<heights.length;i++){
-            for(int j=0;j<heights[0].length;j++){
+        int n=heights.length;
+        int m=heights[0].length;
+        int dist[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 dist[i][j]=Integer.MAX_VALUE;
             }
         }
         dist[0][0]=0;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.wt-b.wt);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.distance-b.distance);
         pq.add(new Pair(0,0,0));
         while(!pq.isEmpty()){
             Pair p=pq.poll();
             int curr_row=p.row;
             int curr_col=p.col;
-            int curr_wt=p.wt;
-            if(curr_row==heights.length-1&&curr_col==heights[0].length-1) return curr_wt;
+            int dis=p.distance;
             for(int i=0;i<4;i++){
                 int new_row=curr_row+rowDirection[i];
                 int new_col=curr_col+colDirection[i];
-                if(isValid(new_row,new_col,heights.length,heights[0].length)){
+                if(isValid(new_row,new_col,n,m)){
                     int diff=Math.abs(heights[new_row][new_col]-heights[curr_row][curr_col]);
-                    int diff1=Math.max(diff,curr_wt);
-                    if(dist[new_row][new_col]>diff1){
-                        dist[new_row][new_col]=diff1;
-                        pq.add(new Pair(new_row,new_col,diff1));
+                    int efforts=Math.max(diff,dist[curr_row][curr_col]);
+                    if(dist[new_row][new_col]>efforts){
+                        dist[new_row][new_col]=efforts;
+                        pq.add(new Pair(new_row,new_col,efforts));
                     }
                 }
             }
         }
-        return 0;
+        return dist[n-1][m-1];
     }
 }
